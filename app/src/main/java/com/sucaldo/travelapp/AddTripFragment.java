19fe006144cd76cc4422d2bfdc40175faf82548f
calderon <hugo.velasquez.le@gmail.com>
2020-05-24 14:26:27 +1000

    First layout of add trips

--- app/src/main/java/com/sucaldo/travelapp/AddTripFragment.java
+++ app/src/main/java/com/sucaldo/travelapp/AddTripFragment.java
@@ -14,5 +14,6 @@ public class AddTripFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.add_trip_view,container,false);
+
     }
 }
