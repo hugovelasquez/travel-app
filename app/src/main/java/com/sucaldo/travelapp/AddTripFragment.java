136b364ad55ea6ecc55913f12195c49ef80561d0
hugovelasquez <hugo.velasquez.le@gmail.com>
2020-05-25 17:33:06 +1000

    Added Trip input fields validation checks before saving
    Co-authored-by: Sofia Calderon <sofia.ester.calderon@gmail.com>

--- app/src/main/java/com/sucaldo/travelapp/AddTripFragment.java
+++ app/src/main/java/com/sucaldo/travelapp/AddTripFragment.java
@@ -5,15 +5,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class AddTripFragment extends Fragment implements View.OnClickListener {
 
@@ -21,6 +28,8 @@ public class AddTripFragment extends Fragment implements View.OnClickListener {
     private DatePickerDialog picker;
     private EditText startDate, endDate;
     private ImageView startDateIcon, endDateIcon;
+    private TextInputEditText fromCountry, fromCity, toCountry, toCity;
+    private Button btnSave;
 
     @Nullable
     @Override
@@ -33,39 +42,109 @@ public class AddTripFragment extends Fragment implements View.OnClickListener {
         endDate = rootView.findViewById(R.id.trip_end_date);
         startDateIcon = rootView.findViewById(R.id.start_date_icon);
         endDateIcon = rootView.findViewById(R.id.end_date_icon);
+        fromCountry = rootView.findViewById(R.id.from_country);
+        toCountry = rootView.findViewById(R.id.to_country);
+        toCity = rootView.findViewById(R.id.to_city);
+        fromCity = rootView.findViewById(R.id.from_city);
+        btnSave = rootView.findViewById(R.id.btn_save);
 
-        // Listener for start and end date
+        // Set listeners
         startDateIcon.setOnClickListener(this);
         endDateIcon.setOnClickListener(this);
+        btnSave.setOnClickListener(this);
 
         return rootView;
     }
 
     @Override
     public void onClick(View v) {
+
+
+        switch (v.getId()) {
+            case R.id.start_date_icon:
+                picker = getPicker(startDate);
+                picker.show();
+                break;
+            case R.id.end_date_icon:
+                picker = getPicker(endDate);
+                picker.show();
+                break;
+            case R.id.btn_save:
+                saveTrip();
+                break;
+        }
+    }
+
+    private void saveTrip() {
+        if (isTripValid()) {
+            Toast.makeText(getContext(), "Saving trip", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isTripValid() {
+        boolean fromCountryValid = checkIfFieldInputIsEmpty(fromCountry);
+        boolean fromCityValid = checkIfFieldInputIsEmpty(fromCity);
+        boolean toCountryValid = checkIfFieldInputIsEmpty(toCountry);
+        boolean toCityValid = checkIfFieldInputIsEmpty(toCity);
+        boolean startAndEndDateValid = validateStartAndEndDate(startDate, endDate);
+        // return true only if all are true
+        return fromCountryValid && fromCityValid && toCountryValid && toCityValid && startAndEndDateValid;
+    }
+
+    private boolean checkIfFieldInputIsEmpty(EditText editText) {
+        if (editText.getText().toString().isEmpty()) {
+            editText.setError(getString(R.string.text_empty_field_error));
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateStartAndEndDate(EditText startDateField, EditText endDateField) {
+        SimpleDateFormat formatter = new SimpleDateFormat("d.M.yyyy");
+        Date startDate;
+        Date endDate;
+
+        // Return false if either start or end Date field are empty
+        if (!checkIfFieldInputIsEmpty(startDateField) | !checkIfFieldInputIsEmpty(endDateField)) {
+            return false;
+        }
+
+        try {
+            startDate = formatter.parse(startDateField.getText().toString());
+
+        } catch (ParseException e) {
+            startDateField.setError(getString(R.string.text_date_format_error));
+            e.printStackTrace();
+            return false;
+        }
+        try {
+            endDate = formatter.parse(endDateField.getText().toString());
+
+        } catch (ParseException e) {
+            endDateField.setError(getString(R.string.text_date_format_error));
+            e.printStackTrace();
+            return false;
+        }
+        if (startDate.after(endDate)) {
+            endDateField.setError(getString(R.string.text_end_date_error));
+            return false;
+        }
+        return true;
+    }
+
+    private DatePickerDialog getPicker(final EditText dateInput) {
         final Calendar cldr = Calendar.getInstance();
         int day = cldr.get(Calendar.DAY_OF_MONTH);
         int month = cldr.get(Calendar.MONTH);
         int year = cldr.get(Calendar.YEAR);
 
-        switch (v.getId()) {
-            case R.id.start_date_icon:
-                picker = getPicker(year, month, day, startDate);
-                picker.show();
-                break;
-            case R.id.end_date_icon:
-                picker = getPicker(year, month, day, endDate);
-                picker.show();
-                break;
-        }
-    }
-
-    private DatePickerDialog getPicker(int year, int month, int day, final EditText dateInput) {
         return new DatePickerDialog(getActivity(),
                 new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                         dateInput.setText(dayOfMonth + "." + (monthOfYear + 1) + "." + year);
+                        // if field not empty, remove error
+                        dateInput.setError(null);
                     }
                 }, year, month, day);
 
